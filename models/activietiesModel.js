bf3b595ae176aee8928033ef71259bb0a5e0839d
dkmak02 <dkmak02@gmail.com>
2023-03-26 16:09:43 +0200

    added opulating to activities model

--- models/activietiesModel.js
+++ models/activietiesModel.js
@@ -1,31 +1,50 @@
 const mongoose = require('mongoose');
 //const validator = require('validator');
 
-const activitySchema = new mongoose.Schema({
-  activity: {
-    type: String,
-    required: [true, 'Must have activity'],
+const activitySchema = new mongoose.Schema(
+  {
+    activity: {
+      type: String,
+      required: [true, 'Must have activity'],
+    },
+    list: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'List',
+      required: [true, 'Activity must belong to a list'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    completedAt: {
+      type: Date,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Activity must belong to a user'],
+    },
   },
-  list: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'List',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  completedAt: {
-    type: Date,
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+activitySchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'email name',
+  });
+  this.populate({
+    path: 'list',
+    select: '-user name',
+  });
+  next();
 });
 
-const ToDo = mongoose.model('ToDo', activitySchema);
-module.exports = ToDo;
+const Activity = mongoose.model('Activity', activitySchema);
+module.exports = Activity;
